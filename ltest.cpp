6b01f24ee700bb3f8afa5c420a870b4ebc5ea224
suvorovroman <suvorovroman@gmail.com>
2018-08-03 19:36:00 +0300

    Добавлен арифметический тест
    Добавлен арифметический тест на последовательности случайных чисел возрастающей длины.

--- ltest.cpp
+++ ltest.cpp
@@ -1,27 +1,58 @@
-/* $Id$ */
+/* Copyright (C) 1996-2018 Suvorov Roman I. */
 #include "lratpp.h"
 #include <iostream>
 #include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
+/** 	\brief Тест основных арифметических операций.
+
+	Проверяет условие a = q*b + r для двух произвольных целых чисел. Здесь
+	q = a/b
+	r = a mod b
+	и b != 0.
+	
+	\param[in]	_l	Максимальная длина исходных данных.
+*/
 static void unit_01(int _l)
 {
 	std::cout << "Arithmetic test. Length " << _l << endl;
-	linteger a = linteger::rand(_l);
-	linteger b = linteger::rand(_l);
-	cout << "a: " << a << '\t' << '[' << a.length() << ']' << endl;
-	cout << "b: " << b << '\t' << '[' << b.length() << ']' << endl;
+	linteger a = linteger::rand(_l);	
+	linteger b;
+	do
+		b = linteger::rand(_l);		
+	while(b == 0);
+	linteger q, r;
+	div(a, b, q, r);
+	if(q*b + r != a)
+	{
+		cout << "a:" << a << '[' << a.length() << ']' << endl;
+		cout << "b:" << b << '[' << b.length() << ']' << endl;
+		cout << "q:" << q << '[' << q.length() << ']' << endl;
+		cout << "r:" << r << '[' << q.length() << ']' << endl;
+		cout << "q*b+r:" << q*b + r << endl;
+		exit(2018);
+	}
 }
+
+#define	MAX_LENGTH	1597
 
 int main(int _argc, const char* _argv[])
 {
+	int f1 = 1, f2 = 1;
 	srand(time(0));
-	unit_01(1);
-	unit_01(2);
-	unit_01(3);
-	unit_01(5);
-	unit_01(10);
+	
+	/* Арифметический тест. Размер чисел растет как последовательность Фиббоначи. */ 
+	do
+	{
+		unit_01(f2);
+		int f = f1 + f2;
+		f1 = f2;
+		f2 = f;
+	}
+	while(f2 < MAX_LENGTH);
+	
 	linteger::finalize();
 	cin.get();
 	return 0;
